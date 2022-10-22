3b81a44d60bda053be76783bf1f9e7722af70603
user <rast.ivan1408@gmail.com>
2022-10-22 09:56:57 +0300

    calculator

---
+++ ds1.py
@@ -0,0 +1,24 @@
+print('нажмите q чтоб выйти')
+while True:
+    s = input('введите знак (+,-,*,/)')
+    if s == 'q': break
+    if s in('+','-','*','/'):
+        x = float(input('x= '))
+        y = float(input('y= '))
+        if s == '+':
+            print("%.2f" % (x+y))
+
+        if s == '-':
+            print("%.2f" % (x-y))
+
+        if s == '*':
+         print("%.2f" % (x*y))
+
+         if s == '/':
+             if y != 0:
+                print("%.2f" % (x/y))
+             else:
+                print("вы делите на ноль")
+    else:
+        print('неверний знак')
+
---
+++ уц.py
@@ -0,0 +1,9 @@
+var_1 = 5
+var_2 = 2
+print(var_1 + var_2)
+print(var_1 - var_2)
+print(var_1 * var_2)
+print(var_1 / var_2)
+print(var_1 ** var_2)
+print(var_1 // var_2)
+print(var_1% var_2)
